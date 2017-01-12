f55c4dc7ea70445bed79df5df67daf20df82d75f
apple <hxc363@163.com>
2017-01-12 16:19:19 +0800

    Initial Commit

---
+++ FrameDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FrameDemo
+//
+//  Created by apple on 17/1/12.
+//  Copyright © 2017年 Master. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FrameDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FrameDemo
+//
+//  Created by apple on 17/1/12.
+//  Copyright © 2017年 Master. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
